93b58e9c180b21e3eda96f9a36522b7d302dfe87
zszzzhy <zszzzhy@gmail.com>
2019-03-09 21:13:26 +0800

    ‘添加ESlint、prettier’

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	root: true,
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly"
+	},
+	parserOptions: {
+		parser: "babel-eslint",
+		ecmaVersion: 2018,
+		sourceType: "module"
+	},
+	env: {
+		browser: true,
+		es6: true,
+		node: true
+	},
+	extends: ["airbnb-base", "plugin:prettier/recommended"],
+	plugins: ["prettier"],
+	rules: {
+		"prettier/prettier": "error",
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,43 +1,41 @@
-import './styles/style.scss';
+import "./styles/style.scss";
 
 new Vue({
-    name: 'notebook',
-
-    el: '#notebook',
-    // 数据
-    data() {
-        return {
-            content: localStorage.getItem('content') || '你可以写 **markdown** ',
-        }
-    },
-    // 计算属性
-    computed: {
-        notePreview() {
-            // Markdown 渲染为 HTML
-            return marked(this.content);
-        },
-    },
-    // 侦听器
-    watch: {
-        content: 'saveNote',
-    },
-    // 通用函数
-    methods: {
-        // 保存笔记
-        saveNote(val, oldval) {
-            console.log('新笔记：', val, '旧笔记：', oldval);
-            console.log('保存笔记：', this.content);
-            localStorage.setItem('content', this.content);
-            this.reportOperation('保存');
-        },
-        // 报告操作
-        reportOperation(opName) {
-            console.log(opName, '操作完成了');
-        },
-    },
-    // 当实例准备就绪就会调用这个钩子
-    // created() {
-    //     this.content = localStorage.getItem('content') || '你可以写 **markdown** ';
-    // }
+	name: "notebook",
+	el: "#notebook",
+	// 数据
+	data() {
+		return {
+			content: localStorage.getItem("content") || "你可以写 **markdown** "
+		};
+	},
+	// 计算属性
+	computed: {
+		notePreview() {
+			// Markdown 渲染为 HTML
+			return marked(this.content);
+		}
+	},
+	// 侦听器
+	watch: {
+		content: "saveNote"
+	},
+	// 通用函数
+	methods: {
+		// 保存笔记
+		saveNote(val, oldval) {
+			console.log("新笔记：", val, "旧笔记：", oldval);
+			console.log("保存笔记：", this.content);
+			localStorage.setItem("content", this.content);
+			this.reportOperation("保存");
+		},
+		// 报告操作
+		reportOperation(opName) {
+			console.log(opName, "操作完成了");
+		}
+	}
+	// 当实例准备就绪就会调用这个钩子
+	// created() {
+	//     this.content = localStorage.getItem('content') || '你可以写 **markdown** ';
+	// }
 });
-
