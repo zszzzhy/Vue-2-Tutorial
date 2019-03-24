a380013d593bc994602810fdeac44d5ce1b5e805
zszzzhy <zszzzhy@dingtalk.com>
2019-03-24 18:24:55 +0800

    '功能完善'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles/style.scss";
 
+Vue.filter("date", time => moment(time).format("DD/MM/YY, HH:mm"));
+
 /* eslint-disable no-new */
 new Vue({
 	name: "notebook",
@@ -7,31 +9,69 @@ new Vue({
 	// 数据
 	data() {
 		return {
-			content: localStorage.getItem("content") || "你可以写 **markdown** ",
-			notes: [],
+			notes: JSON.parse(localStorage.getItem("notes")) || [],
 			// 选中笔记的 ID
-			selectedId: null
+			selectedId: localStorage.getItem("selected-id") || null
 		};
 	},
 	// 计算属性
 	computed: {
+		selectedNote() {
+			// 返回与 selectedId 匹配的笔记
+			return this.notes.find(note => note.id === this.selectedId);
+		},
 		notePreview() {
 			// Markdown 渲染为 HTML
 			return this.selectedNote ? marked(this.selectedNote.content) : "";
 		},
-		addButtonTitle() {
-			// 提示笔记数
-			return notes.length + " note(s) already";
+		sortedNotes() {
+			// 排序笔记
+			return this.notes
+				.slice()
+				.sort((a, b) => a.created - b.created)
+				.sort((a, b) => {
+					return a.favorite === b.favorite ? 0 : a.favorite ? -1 : 1;
+				});
 		},
-		selectedNote() {
-			// 返回与 selectedId 匹配的笔记
-			return this.notes.find(note => note.id === this.selectedId);
+		linesCount() {
+			// 计算行数
+			if (this.selectedNote) {
+				// 计算换行符的个数
+				return this.selectedNote.content.split(/\r\n|\r|\n/).length;
+			}
+		},
+		wordsCount() {
+			// 计算单词数
+			if (this.selectedNote) {
+				let s = this.selectedNote.content;
+				// 将换行符转换为空格
+				s = s.replace(/\n/g, " ");
+				// 排除开头和结尾的空格
+				s = s.replace(/(^\s*)|(\s*$)/gi, "");
+				// 将多个重复空格转换为一个
+				s = s.replace(/\s\s+/gi, " ");
+				// 返回空格数量
+				return s.split(" ").length;
+			}
+		},
+		charactersCount() {
+			// 计算字符数
+			if (this.selectedNote) {
+				return this.selectedNote.content.split("").length;
+			}
 		}
 	},
 	// 侦听器
 	watch: {
-		content: "saveNote",
-		notes: "saveNotes"
+		notes: {
+			// 方法名
+			handler: "saveNotes",
+			// 需要使用这个选项来侦听数组中每个笔记属性的变化
+			deep: true
+		},
+		selectedId(val, oldVal) {
+			localStorage.setItem("selected-id", val);
+		}
 	},
 	// 通用函数
 	methods: {
@@ -42,37 +82,38 @@ new Vue({
 			const note = {
 				id: String(time),
 				title: `New note ${this.notes.length + 1}`,
-				content:
-					"**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!",
+				content: "**你好！** 这条笔记是使用 [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) 编写！",
 				created: time,
 				favorite: false
 			};
 			// 添加到列表中
 			this.notes.push(note);
+			// 选中新建笔记
+			this.selectNote(note);
 		},
-		// 保存笔记
-		saveNote(val, oldval) {
-			console.log("新笔记：", val, "旧笔记：", oldval);
-			console.log("保存笔记：", this.content);
-			localStorage.setItem("content", this.content);
-			this.reportOperation("保存");
-		},
-		// 报告操作
-		reportOperation(opName) {
-			console.log(opName, "操作完成了");
+		// 移除笔记
+		removeNote() {
+			if (this.selectedNote && confirm("是否删除笔记？")) {
+				// 将选中的笔记从笔记列表中移除
+				const index = this.notes.indexOf(this.selectedNote);
+				if (index !== -1) {
+					this.notes.splice(index, 1);
+				}
+			}
 		},
 		// 选择笔记
 		selectNote(note) {
 			this.selectedId = note.id;
 		},
+		// 保存笔记
 		saveNotes() {
 			// 在存储之前不要忘记把对象转换为 JSON 字符串
 			localStorage.setItem("notes", JSON.stringify(this.notes));
 			console.log("Notes saved!", new Date());
+		},
+		// 收藏笔记
+		favoriteNote() {
+			this.selectedNote.favorite ^= true;
 		}
 	}
-	// 当实例准备就绪就会调用这个钩子
-	// created() {
-	//     this.content = localStorage.getItem('content') || '你可以写 **markdown** ';
-	// }
 });
